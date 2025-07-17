d19b72bab63f7fa4e8dfae6e5664073a8466cc58
Stefan Meinecke <meinecke@greensec.de>
2025-07-18 00:04:54 +0200

    update repository-name

--- add-repository.sh
+++ add-repository.sh
@@ -7,6 +7,6 @@ fi
 
 ${SUDO} apt-get update
 ${SUDO} apt-get -y install lsb-release ca-certificates wget
-${SUDO} wget -O /usr/share/keyrings/greensec.github.io-nsd-deb-build.key https://greensec.github.io/nsd-deb-build/public.key
-echo "deb [signed-by=/usr/share/keyrings/greensec.github.io-nsd-deb-build.key] https://greensec.github.io/nsd-deb-build/repo $(lsb_release -sc) main" | ${SUDO} tee /etc/apt/sources.list.d/nsd-deb-build.list
+${SUDO} wget -O /usr/share/keyrings/greensec.github.io-nsd-debian.key https://greensec.github.io/nsd-debian/public.key
+echo "deb [signed-by=/usr/share/keyrings/greensec.github.io-nsd-debian.key] https://greensec.github.io/nsd-debian/repo $(lsb_release -sc) main" | ${SUDO} tee /etc/apt/sources.list.d/nsd-debian.list
 ${SUDO} apt-get update
